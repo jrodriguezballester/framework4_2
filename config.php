95eb789119f379525e81f4901787f4234642e9c8
JOSE ANTONIO RODRIGUEZ BALLESTER <34791843l@AD.FPMISLATA.COM>
2019-10-25 08:45:16 +0200

    Agregar Error y config

--- config.php
+++ config.php
@@ -2,6 +2,21 @@
 
 return array(
     "site" => array(
-        "root" => "http://localhost:8080/DWS/framework/4",
+        "root" => "http://localhost/DWS/framework4",
+        "subdomain" => "/DWS/framework4",
+        "rutaCSS" => "http://localhost/DWS/framework4/public/css/",
+        "rutaIMAGES" => "http://localhost/DWS/framework4/public/images/",
+        "rutaABSOLUTA" => "C:/xampp/htdocs/DWS/framework4/public/vistas/"
+
     ),
+ //$this->bbdd=new PdoConnection("CONNECTION:host=HOST;dbname=NAMEDB","USERNAME","PASSWORD");   
+    "DB" => array(
+        "CONNECTION"=>"mysql",
+        "HOST"=>"host",
+        "PORT" => "3306",
+        "USERNAME"=>"root",
+        "PASSWORD" => "",  
+        "NAMEDB" => "nba",
+ 
+    )
 );
